9e90e2f4e284795c6a8fe1d91ec294406cdabb41
Ema93sh <m.magesh.66@gmail.com>
2019-02-27 15:36:45 -0700

    Added hystrix

--- Components/Backlog/ProjectClient.cs
+++ Components/Backlog/ProjectClient.cs
@@ -1,25 +1,47 @@
 ﻿using System.Net.Http;
 using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 
 namespace Backlog
 {
     public class ProjectClient : IProjectClient
     {
+        private Dictionary<long, ProjectInfo> _projectCache = new Dictionary<long, ProjectInfo>();
         private readonly HttpClient _client;
+        private readonly ILogger<ProjectClient> _logger;
 
-        public ProjectClient(HttpClient client)
+        public ProjectClient(HttpClient client, ILogger<ProjectClient> logger)
         {
             _client = client;
+            _logger = logger;
         }
 
         public async Task<ProjectInfo> Get(long projectId)
         {
+            return await new GetProjectCommand(DoGet, DoGetFromCache, projectId).ExecuteAsync();
+        }
+
+        public async Task<ProjectInfo> DoGet(long projectId)
+        {
             _client.DefaultRequestHeaders.Accept.Clear();
             var streamTask = _client.GetStreamAsync($"project?projectId={projectId}");
 
+            _logger.LogInformation($"Attempting to fetch projectId: {projectId}");
             var serializer = new DataContractJsonSerializer(typeof(ProjectInfo));
-            return serializer.ReadObject(await streamTask) as ProjectInfo;
+            ProjectInfo pi = (ProjectInfo)serializer.ReadObject(await streamTask);
+
+            _projectCache.Add(projectId, pi);
+            _logger.LogInformation($"Caching projectId: {projectId}");
+
+            return pi;
+        }
+
+        public Task<ProjectInfo> DoGetFromCache(long projectId)
+        {
+            _logger.LogInformation($"Retrieving from cache projectId: {projectId}");
+            return Task.FromResult(_projectCache[projectId]);
         }
     }
 }
